76fb71ea5ff89c0e797ba6b925db157363aacbb4
Kemal Yildirim <kemalyildirm@gmail.com>
2022-12-13 10:49:48 +0300

    remove themeprovider for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from "react";
-import {
-  Box,
-  createTheme,
-  CssBaseline,
-  Paper,
-  ThemeProvider,
-} from "@mui/material";
+import { Box, createTheme, CssBaseline } from "@mui/material";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { routes } from "./routes";
 import Sidebar from "./components/Sidebar";
@@ -29,31 +23,29 @@ function App() {
   });
 
   return (
-    <ThemeProvider theme={theme}>
-      <div>
-        <CssBaseline />
-        <Sidebar />
-        <Box
-          height="100vh"
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          flexDirection="column"
-        >
-          <BrowserRouter>
-            <Routes>
-              {routes.map((route) => (
-                <Route
-                  key={route.key}
-                  path={route.path}
-                  element={<route.component />}
-                />
-              ))}
-            </Routes>
-          </BrowserRouter>
-        </Box>
-      </div>
-    </ThemeProvider>
+    <div>
+      <CssBaseline />
+      <Sidebar />
+      <Box
+        height="100vh"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        flexDirection="column"
+      >
+        <BrowserRouter>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.key}
+                path={route.path}
+                element={<route.component />}
+              />
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </Box>
+    </div>
   );
 }
 // {/* <script src="https://docs.opencv.org/4.5.4/opencv.js"></script> */}
