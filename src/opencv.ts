e3713516551edcf50f44bbce24c41191a94ebe51
Kemal Yildirim <kemalyildirm@gmail.com>
2022-12-13 21:48:19 +0300

    importing opencv.js

---
+++ src/opencv.ts
@@ -0,0 +1,16 @@
+// TODO: Test the import methods
+
+const OPENCV_SCRIPT_LINK="https://docs.opencv.org/4.5.4/opencv.js"
+export var cv2 = require(OPENCV_SCRIPT_LINK)
+// function mountScript (scriptToAppend: string) {
+//     const script = document.createElement("script");
+//     script.src = scriptToAppend;
+//     script.async = true;
+//     document.body.appendChild(script);
+// }
+
+// function LoadOpenCV() {
+//     mountScript(OPENCV_SCRIPT_LINK)
+// }
+
+// export default LoadOpenCV;
